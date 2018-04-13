61eee765c85ab0eeaf25fe4777cfd7a91aed2d0f
Guilherme Pangnotta <setetres@gmail.com>
2018-04-12 22:00:01 -0300

    Remove trash

--- src/js/main.js
+++ src/js/main.js
@@ -244,10 +244,10 @@
         page('/cacete-company/', cacete);
         page('/contact/', contact);
         page('/inkstarter/', inkstarter);
-        page();
 
         $(window).on('load', function() {
             pageLoaded();
+            page();
         });
 
         $(window).on('debouncedresize', function(){
